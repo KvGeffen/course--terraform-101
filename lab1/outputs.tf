56fd79f1741366b3c4eca6baab046198c910b62e
Koen van Geffen <k.vangeffen@enzazaden.nl>
2025-07-15 17:23:38 +0200

    wip

--- lab1/outputs.tf
+++ lab1/outputs.tf
@@ -1,11 +1,12 @@
 output "environment_prefix" {
   value = local.environment_prefix
 }
-
 output "environment_name" {
   value = var.environment_name
 }
-
 output "application_name" {
   value = var.application_name
+}
+output "suffix" {
+  value = random_string.suffix.result
 }
