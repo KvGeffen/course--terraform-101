4ce8dd5ea83483d0f421a23bfe17cfb11b5ca096
Koen van Geffen <k.vangeffen@enzazaden.nl>
2025-07-16 11:13:25 +0200

    save

--- lab2/main.tf
+++ lab2/main.tf
@@ -1,4 +1,24 @@
-resource "azurerm_resource_group" "kvg_main" {
+resource "azurerm_resource_group" "main" {
   name     = "kvg-rg-${var.application_name}-${var.environment_name}"
   location = var.primary_location
+}
+
+resource "random_string" "suffix" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "kvgst${random_string.suffix.result}001"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
 }
