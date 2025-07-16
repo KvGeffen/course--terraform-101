45ab1e579988388057efb5f8acfe50b741fde17b
Koen van Geffen <k.vangeffen@enzazaden.nl>
2025-07-16 14:26:17 +0200

    save

---
+++ lab4/main.tf
@@ -0,0 +1,21 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "main" {
+  name     = "kvg-rg-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
+}
+
+resource "random_string" "keyvault_suffic" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "azurerm_key_vault" "main" {
+  name                = "kvg-kv-${var.application_name}-${var.environment_name}-${random_string.keyvault_suffic.result}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+}
+
