e6064b51b5bbbf578bab74b11da7a60ec0c3235d
Koen van Geffen <k.vangeffen@enzazaden.nl>
2025-07-16 13:02:19 +0200

    save

---
+++ lab3/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "main" {
+  name     = "kvg-rg-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "kvg-law-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# resource "random_string" "suffix" {
+#   length  = 10
+#   special = false
+#   upper   = false
+# }
+
+# resource "azurerm_storage_account" "main" {
+#   name                     = "kvgst${random_string.suffix.result}001"
+#   resource_group_name      = azurerm_resource_group.main.name
+#   location                 = azurerm_resource_group.main.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+# }
+
+# resource "azurerm_storage_container" "main" {
+#   name                  = "tfstate"
+#   storage_account_id    = azurerm_storage_account.main.id
+#   container_access_type = "private"
+# }
